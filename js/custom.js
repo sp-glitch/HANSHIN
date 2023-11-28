3c954a810db59ea35a3761ebc78850713cc897b6
sp-glitch <148739294+sp-glitch@users.noreply.github.com>
2023-11-28 18:02:11 +0900

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+$(function () {
+  $(".visual_slide").slick({
+    arrows: false,
+    autoplay: true,
+    pauseOnHover: false,
+  });
+
+  $(".main_right_slide").slick({
+    arrows: true,
+    dots: true,
+  });
+});
